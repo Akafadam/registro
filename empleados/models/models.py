17e875370031eefcf64fe6cb72fdd730c93e8682
Ricardo <ricardoch2000@gmail.com>
2021-04-27 12:04:44 -0400

    [EDIT]Agregar atributos para el modulo "empleados"

--- empleados/models/models.py
+++ empleados/models/models.py
@@ -2,13 +2,19 @@
 
 from odoo import models, fields, api
 
-# class empleados(models.Model):
-#     _name = 'empleados.empleados'
+class empleados(models.Model):
+    _name = 'empleados.empleados'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char(string="Nombre", required=True)
+    date = fields.Datetime(string="Fecha", required=True)
+    birthyear = fields.Integer(string="Año de nacimiento", required=True)
+    phone = fields.Integer(string="Número telefónico", required=True)
+    email = fields.Char(string="Correo eletrónico", required=True)
+    address = fields.Char(string="Dirección", required=True)
+    charge = fields.Char(string="Cargo", required=True)
+    speciality = fields.Char(string="Especialidad", required=True)
+    worked = fields.Char(string="Horas trabajadas", required=True)
+    pay = fields.Char(string="Remuneración", required=True)
 #
 #     @api.depends('value')
 #     def _value_pc(self):
