2eeeebf6f40aa7d8bc6c608f87a369f642650211
Ricardo <ricardoch2000@gmail.com>
2021-04-27 10:53:59 -0400

    [ADD]Crear estructura de paquetes [EDIT]Crear atributos para el modulo "clientes"

---
+++ clientes/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class clientes(models.Model):
+    _name = 'clientes.clientes'
+
+    name = fields.Char(string="Nombre", required=True)
+    id_card = fields.Integer(string="CI", required=True)
+    date = fields.Datetime(string="Fecha", required=True)
+    birthyear = fields.Integer(string="Año de nacimiento", required=True)
+    phone = fields.Integer(string="Número telefónico", required=True)
+    email = fields.Char(string="Correo eletrónico", required=True)
+    address = fields.Char(string="Dirección", required=True)
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
