9fa8be8366d5dae9fb48a37d8357ef74a2b71993
Ricardo <ricardoch2000@gmail.com>
2021-04-28 11:43:19 -0400

    [EDIT]Quitar "fecha" de clientes y pacientes [EDIT]Agregar atributos para "inventario" y "producto"

--- inventarios/models/models.py
+++ inventarios/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class inventarios(models.Model):
-#     _name = 'inventarios.inventarios'
+class inventarios(models.Model):
+    _name = 'inventarios.inventarios'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    product = fields.Char(string="Producto", required=True)
+    reserve_type = fields.Char(string="Tipo de Reserva", required=True)
+    cuantity = fields.Integer(string="Cantidad", required=True)
+    date = fields.Datetime(string="Fecha", required=True)
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
--- productos/models/models.py
+++ productos/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class productos(models.Model):
-#     _name = 'productos.productos'
+class productos(models.Model):
+    _name = 'productos.productos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    name = fields.Char(string="Nombre", required=True)
+    code = fields.Integer(string="Código", required=True)
+    cost = fields.Integer(string="Costo", required=True)
+    qr_code = fields.Binary(string="Código QR", required=True)
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
