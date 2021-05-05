4c40c4bd76209c850e5590b1aa594fd810624337
Ricardo <ricardoch2000@gmail.com>
2021-05-05 10:49:50 -0400

    [EDIT]Instalando modulo "citas"

--- citas/models/models.py
+++ citas/models/models.py
@@ -2,13 +2,21 @@
 
 from odoo import models, fields, api
 
-# class citas(models.Model):
-#     _name = 'citas.citas'
+class citas(models.Model):
+    _name = 'citas.citas'
+    date_time = fields.Datetime(string="Fecha y Hora", required=True)
+    client_data = fields.Many2one('clientes.clientes', string="Datos del cliente", required=True)
+    pacient_data = fields.Many2one('pacientes.pacientes', string="Datos del paciente", required=True)
+    speciality = fields.Selection([('geriatra', 'Geriatra'),
+                                    ('pediatra', 'Pediatra'),
+                                    ('cardiologo', 'Cardiologo'),
+                                    ('hematologo', 'Hematologo'),
+                                    ('neurologo', 'Neurologo'),
+                                    ('nutriologo', 'Nutriologo'),
+                                    ('traumatologo', 'Traumatologo')],
+    string="Especialidad Medica", required=True)
+    medic_data = fields.Many2one('empleados.empleados', string="Medico", required=True)
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
