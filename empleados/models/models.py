98e2faa662ea4af7d2314abd3b14000f72edcb72
Raúl Chirinos <raul1998@gmail.com>
2021-05-09 17:37:23 -0400

    [EDIT] Quitando menus y poniendo paginas en su lugar

--- empleados/models/models.py
+++ empleados/models/models.py
@@ -2,17 +2,19 @@
 
 from odoo import models, fields, api
 
+
 class empleados(models.Model):
     _name = 'empleados.empleados'
 
-    _sql_constraints = [('employees_record','UNIQUE(id_card)', 'Este usuario ya esta registrado')]
+    _sql_constraints = [
+        ('employees_record', 'UNIQUE(id_card)', 'Este usuario ya esta registrado')]
 
     name = fields.Char(string="Nombre", required=True)
-    birthyear = fields.Datetime(string="Año de nacimiento", required=True)
+    birthyear = fields.Date(string="Año de nacimiento", required=True)
     charge = fields.Selection([('medico', 'Medico'),
                               ('servicio', 'Servicio'),
                               ('administrativo', 'Administrativo')],
-    string="Cargo")
+                              string="Cargo")
     phone = fields.Integer(string="Número telefónico", required=True)
     id_card = fields.Integer(string="Cédula", required=True)
     email = fields.Char(string="Correo eletrónico", required=True)
@@ -25,25 +27,25 @@ class empleados(models.Model):
                                     ('15:00', '15:00'),
                                     ('16:00', '16:00'),
                                     ('17:00', '17:00')],
-    string="Hora de Llegada")
-    leave_time = fields.Selection( [('09:00', '09:00'),
-                                    ('10:00', '10:00'),
-                                    ('11:00', '11:00'),
-                                    ('12:00', '12:00'),
-                                    ('13:00', '13:00'),
-                                    ('14:00', '14:00'),
-                                    ('15:00', '15:00'),
-                                    ('16:00', '16:00'),
-                                    ('17:00', '17:00')],
-    string="Hora de salida")
+                                   string="Hora de Llegada")
+    leave_time = fields.Selection([('09:00', '09:00'),
+                                   ('10:00', '10:00'),
+                                   ('11:00', '11:00'),
+                                   ('12:00', '12:00'),
+                                   ('13:00', '13:00'),
+                                   ('14:00', '14:00'),
+                                   ('15:00', '15:00'),
+                                   ('16:00', '16:00'),
+                                   ('17:00', '17:00')],
+                                  string="Hora de salida")
     address = fields.Char(string="Dirección", required=True)
     speciality = fields.Selection([('geriatra', 'Geriatra'),
-                                    ('pediatra', 'Pediatra'),
-                                    ('cardiologo', 'Cardiologo'),
-                                    ('hematologo', 'Hematologo'),
-                                    ('neurologo', 'Neurologo'),
-                                    ('nutriologo', 'Nutriologo'),
-                                    ('traumatologo', 'Traumatologo')],
-    string="Especialidad")
+                                   ('pediatra', 'Pediatra'),
+                                   ('cardiologo', 'Cardiologo'),
+                                   ('hematologo', 'Hematologo'),
+                                   ('neurologo', 'Neurologo'),
+                                   ('nutriologo', 'Nutriologo'),
+                                   ('traumatologo', 'Traumatologo')],
+                                  string="Especialidad")
     worked = fields.Char(string="Horas trabajadas", required=True)
     pay = fields.Char(string="Remuneración", required=True)
--- pacientes/models/models.py
+++ pacientes/models/models.py
@@ -2,14 +2,16 @@
 
 from odoo import models, fields, api
 
+
 class pacientes(models.Model):
     _name = 'pacientes.pacientes'
 
-    _sql_constraints = [('pacients_record','UNIQUE(id_card)', 'Este usuario ya esta registrado')]
+    _sql_constraints = [
+        ('pacients_record', 'UNIQUE(id_card)', 'Este usuario ya esta registrado')]
 
     name = fields.Char(string="Nombre", required=True)
     id_card = fields.Integer(string="CI", required=True)
-    birthyear = fields.Datetime(string="Año de nacimiento", required=True)
+    birthyear = fields.Date(string="Año de nacimiento", required=True)
     phone = fields.Integer(string="Número telefónico", required=True)
     email = fields.Char(string="Correo eletrónico", required=True)
     address = fields.Char(string="Dirección", required=True)
