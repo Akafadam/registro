ca07f3720649dff738c087cd1b7f190707884f1e
Raúl Chirinos <raul1998@gmail.com>
2021-05-10 19:05:55 -0400

    Trabajando en la deteccion fechas en el modulo de citas

--- citas/models/models.py
+++ citas/models/models.py
@@ -1,37 +1,42 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from time import strftime
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+
 
 class citas(models.Model):
     _name = 'citas.citas'
 
-    _sql_constraints = [
-        ('revision_record_id_card','UNIQUE(date_time)', 'Esta hora ya esta registrada'),
-        ('revision_record_date_time','CHECK(date_time>current_date)', 'Esta fecha y hora ya pasaron')
-    ]
+    # _sql_constraints = [
+    #     ('revision_record_id_card', 'UNIQUE(date_time)',
+    # 'Esta hora ya esta registrada')]
 
+    @api.constrains('date_time')
+    def _check_schedule(self):
+        if self.date_time < datetime.now():
+            raise ValueError('Esta fecha y hora ya pasaron')
 
-    # def check_schedule(self):
-    #     for data1 in self:
-    #         time1 = data1.date_time[-5:]
-    #         for data2 in self:     
-    #             time2 = date2.date_time[-5:]
-    #             if time1 == time2:
-
+        timediff = relativedelta(self.date_time, datetime.now())
+        hourdiff = timediff.hours
+        if hourdiff < 1:
+            raise ValueError('Esta hora ya está reservada')
 
     date_time = fields.Datetime(string="Fecha y Hora", required=True)
-    client_data = fields.Many2one('clientes.clientes', string="Datos del cliente", required=True)
-    pacient_data = fields.Many2one('pacientes.pacientes', string="Datos del paciente", required=True)
+    client_data = fields.Many2one(
+        'clientes.clientes', string="Datos del cliente", required=True)
+    pacient_data = fields.Many2one(
+        'pacientes.pacientes', string="Datos del paciente", required=True)
     speciality = fields.Selection([('geriatra', 'Geriatra'),
-                                    ('pediatra', 'Pediatra'),
-                                    ('cardiologo', 'Cardiologo'),
-                                    ('hematologo', 'Hematologo'),
-                                    ('neurologo', 'Neurologo'),
-                                    ('nutriologo', 'Nutriologo'),
-                                    ('traumatologo', 'Traumatologo')],
-    string="Especialidad Medica", required=True)
-    medic_data = fields.Many2one('empleados.empleados', string="Medico", required=True)
+                                   ('pediatra', 'Pediatra'),
+                                   ('cardiologo', 'Cardiologo'),
+                                   ('hematologo', 'Hematologo'),
+                                   ('neurologo', 'Neurologo'),
+                                   ('nutriologo', 'Nutriologo'),
+                                   ('traumatologo', 'Traumatologo')],
+                                  string="Especialidad Medica", required=True)
+    medic_data = fields.Many2one(
+        'empleados.empleados', string="Medico", required=True)
 
 
 #
