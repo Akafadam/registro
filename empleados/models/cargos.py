c6fa7b13a7c0bc3c9f161e2b2872f1a4ebcea406
Ricardo <ricardoch2000@gmail.com>
2021-05-21 08:47:23 -0400

    [EDIT]Agregando tipo de cargo

--- empleados/models/cargos.py
+++ empleados/models/cargos.py
@@ -21,17 +21,25 @@ class cargos(models.Model):
     #         raise UserError('Este cargo no se puede editar')
     #     return super(cargos, self).write(value)
 
-    @api.onchange('is_medic')
+    @api.onchange('type')
     # @api.depends('is_medic')
     @api.multi
     def _set_medic(self):
-        if self.is_medic:
+        if self.type == 'medico':
+            self.is_medic = True
+            self.speciality = True
+        elif self.type == 'administrativo':
+            self.is_medic = False
             self.speciality = True
         else:
+            self.is_medic = False
             self.speciality = False
 
     name = fields.Char(string="Cargos",
                        readonly=False, required=True)
-    speciality = fields.Boolean(
-        string="Especialidad Requerida")
-    is_medic = fields.Boolean(string="¿Es medico?")
+    type = fields.Selection([('medico', 'Medico'),
+                             ('servicio', 'Servicio'),
+                             ('administrativo', 'Administrativo')],string="Tipo de cargo")
+    is_medic = fields.Boolean(string="Es medico")
+    speciality = fields.Boolean(string="Especialidad Requerida")
+
