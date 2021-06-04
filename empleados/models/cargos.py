f0a17775fa1ba08c029cf67f6ad600ed23f6f132
Raúl Chirinos <raul1998@gmail.com>
2021-06-04 03:16:28 -0400

    [EDIT]No mostrar boton de invalidar cuando sea medico

--- empleados/models/cargos.py
+++ empleados/models/cargos.py
@@ -11,7 +11,7 @@ class cargos(models.Model):
 
     @api.multi
     def unlink(self):
-        # data = self[0]
+
         for rec in self:
             if rec.state == "accepted":
                 raise UserError(
@@ -29,41 +29,9 @@ class cargos(models.Model):
 
     def invalidate(self):
         super(cargos, self).write({'state': 'draft'})
-        # self.state = 'draft'
-
-    # @api.multi
-    # def unlink(self):
-    #     if self.name == 'Medico':
-    #         raise UserError('Este cargo no se puede borrar')
-    #     return super(cargos, self).unlink()
-
-    # @api.multi
-    # def write(self, value):
-    #     if self.name == 'Medico':
-    #         raise UserError('Este cargo no se puede editar')
-    #     return super(cargos, self).write(value)
-
-    # @api.onchange('subtype')
-    # # @api.depends('is_medic')
-    # @api.multi
-    # def _set_medic(self):
-    #     if self.subtype == 'medico':
-    #         self.is_medic = True
-    #         self.speciality = True
-    #     elif self.subtype == 'administrativo':
-    #         self.is_medic = False
-    #         self.speciality = True
-    #     else:
-    #         self.is_medic = False
-    #         self.speciality = False
 
     name = fields.Char(string="Cargos",
                        readonly=False, required=True)
-    # subtype = fields.Selection([('medico', 'Medico'),
-    #                             ('servicio', 'Servicio'),
-    #                             ('administrativo', 'Administrativo')], string="Tipo de cargo")
-    # is_medic = fields.Boolean(string="Es medico", readonly=True)
-    # speciality = fields.Boolean(string="Especialidad Requerida", readonly=True)
     state = fields.Selection([
         ('draft', 'Borrador'),
         ('accepted', 'Validado')
