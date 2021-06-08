5811b1848881fd0794c54a60f4ac6bea9a26db4f
Ricardo <ricardoch2000@gmail.com>
2021-06-08 11:17:35 -0400

    [EDIT]Tomando solo fechas superiores o iguales al intervalo

--- empleados/models/asistencias.py
+++ empleados/models/asistencias.py
@@ -86,23 +86,23 @@ class asistencias(models.Model):
    #     date = datetime.date.today()
    #     return date.replace(month=month, year=year, day=1)
 
-    @api.constrains('date')
-    def _check_interval(self):
-        current_year = date.today().year
-        current_month = date.today().month
-        if  date.today() < date(current_year, current_month, 15):
-            print('\033[94m' + f'{current_year}')
-            print('\033[91m' + f'{current_month}')
-            print('\033[93m' + f'{self.date.today()}')
-            if self.date < self._last_day(current_year, current_month - 1):
-                raise UserError('La fecha es inferior al intervalo')
-            if self.date > date(current_year, current_month, 15):
-                raise UserError('La fecha es superior al intervalo')
-        if date.today() >= date(current_year, current_month, 15):
-            if self.date < date(current_year, current_month, 15):
-                raise UserError('La fecha es inferior al intervalo')
-            if self.date > self._last_day(current_year, current_month):
-                raise UserError('La fecha es superior al intervalo')
+    # @api.constrains('date')
+    # def _check_interval(self):
+    #     current_year = date.today().year
+    #     current_month = date.today().month
+    #     if  date.today() < date(current_year, current_month, 15):
+    #         print('\033[94m' + f'{current_year}')
+    #         print('\033[91m' + f'{current_month}')
+    #         print('\033[93m' + f'{self.date.today()}')
+    #         if self.date < self._last_day(current_year, current_month - 1):
+    #             raise UserError('La fecha es inferior al intervalo')
+    #         if self.date > date(current_year, current_month, 15):
+    #             raise UserError('La fecha es superior al intervalo')
+    #     if date.today() >= date(current_year, current_month, 15):
+    #         if self.date < date(current_year, current_month, 15):
+    #             raise UserError('La fecha es inferior al intervalo')
+    #         if self.date > self._last_day(current_year, current_month):
+    #             raise UserError('La fecha es superior al intervalo')
 
     @api.multi
     def write(self, vals):
--- empleados/models/nomina.py
+++ empleados/models/nomina.py
@@ -1,5 +1,8 @@
 from odoo import models, fields, api
+import calendar
 from odoo.exceptions import UserError, ValidationError
+from datetime import date, datetime
+
 class nomina(models.Model):
     _name = 'empleados.nomina'
 
@@ -7,21 +10,37 @@ class nomina(models.Model):
     # def write(self, vals):
     #     return super(nomina, self).write(vals)
 
+    def _last_day(self, year, month):
+        last_day = calendar.monthrange(year, month)[1]
+        print('\033[94m' + f"{last_day}")
+        return date(year, month, last_day)
+
     @api.model
     def fill_rows(self):
+        current_year = date.today().year
+        current_month = date.today().month
+        start = 0
+
+        if date.today().day < 15:
+            start = self._last_day(current_year, current_month - 1)
+        else:
+            start = date(current_year, current_month, 15)
+
         for rec in self.env['empleados.empleados'].search([('state','=','accepted')]):
             worked_hours = 0
             pay = 0
-            for item in rec.attendance:
+
+            for item in rec.attendance.search([('date','>=',start)]):
                 worked_hours += item.worked_hours
                 pay += item.pay
+
             vals2 = {
                 'employee':rec.name,
                 'id_card' : rec.id_card,
                 'worked_hours' : worked_hours,
                 'pay' : pay
             }
-            # vals2['employee'] = rec.name
+            print(vals2)
             row = self.env['empleados.nomina'].search([('id_card','=',rec.id_card)])
             if row:
                 super(nomina, row).write(vals2)
@@ -30,6 +49,7 @@ class nomina(models.Model):
 
     employee = fields.Char(string="Empleados")
     id_card = fields.Integer(string="Cedula")
+    start = fields.Date()
     worked_hours = fields.Integer(string="Horas Trabajadas")
     pay = fields.Integer(string="Remuneracion")
     state = fields.Selection([
